9d6b646996b2438be31d4146fca704b98fbf6d0b
drdim <drdim@hostel6.ru>
2012-03-17 22:19:17 +0600

    sd

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ var secret = fs.readFileSync(__dirname + '/secret.txt', 'utf8').trim();
 var launchedVLC = [];
 var userVotes = {}; // uid => url
 var lifeTime = 30000;//?
-var vlcLimit = 6;
+var vlcLimit = 7;
 
 var freePorts = new FIFO();/* свободные порты, на которых будут потоки */
 
@@ -148,7 +148,7 @@ function work() {
     (function (y) {
       util.puts('launching vlc with url: ' + y.url);
 //      y.process = spawn('cvlc', ['--sout-http-mime=video/mpeg','--http-caching=1200', '--sout', '#transcode{vcodec=h264,vb=576,width=360,height=288,acodec=mp4a,ab=96,channels=2}:std{access=http,mux=ts,dst=:' + y.port + '}', y.url,'vlc://quit']);
-      y.process = spawn('cvlc', ['--sout-http-mime=video/mpeg','--http-caching=1200', '--sout', '#transcode{vcodec=h264,vb=400,scale=0.5,acodec=mp3,ab=96,channels=2}:std{access=http,mux=ts,dst=:' + y.port + '}', y.url,'vlc://quit']);
+      y.process = spawn('cvlc', ['--sout-http-mime=video/mpeg','--http-caching=1200', '--sout', '#transcode{vcodec=h264,vb=284,scale=0.5,acodec=a52,ab=64,channels=2}:std{access=http,mux=ts,dst=:' + y.port + '}', y.url,'vlc://quit']);
       emitter.emit('vlcEvent', {url: y.url, outputURL: y.outputURL});
       y.process.on('exit', function (code) {
         var r = launchedVLC.indexOf(y);
